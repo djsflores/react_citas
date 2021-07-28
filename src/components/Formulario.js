1d9190a2626ff64e6680827a126c48fc400e7365
David <david.jesus.sebastian.flores@gmail.com>
2021-07-27 21:06:52 -0300

    evento leer cuando el usuario envia el formulario y validacion de los inputs

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -11,8 +11,10 @@ const Formulario = () => {
         sintomas: ''
     });
 
+    const [error, actualizarError] = useState(false);
+
     // Función que se ejecuta cada que el usuario escribe un input
-    const actualizarState = (e) => {
+    const actualizarState = e => {
         actualizarCita({
             ...cita,
             [e.target.name] : e.target.value
@@ -22,10 +24,37 @@ const Formulario = () => {
     // Extraer los valores
     const { mascota, propietario, fecha, hora, sintomas} = cita;
 
+    // Cuando el usuario presiona agregar cita
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+        if(mascota.trim() === '' || propietario.trim() === '' || 
+        fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === ''){
+            actualizarError(true);
+            return;
+        } else {
+            actualizarError(false);
+        }
+
+        // Asignar un ID
+
+        // Crear la cita
+
+        // Reiniciar el form
+
+
+    }
+
+
     return (  
         <Fragment>
             <h2>Crear Cita</h2>
-            <form>
+            { error? <p className="alerta-error">Todos los campos son obligatorios</p> 
+            : null}
+            <form
+                onSubmit={submitCita}
+            >
                 <label>Nombre Mascota</label>
                 <input 
                     type="text"
